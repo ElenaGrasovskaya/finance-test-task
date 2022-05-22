15424facdb4197792587a39552eea4def8b81de1
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2022-05-23 00:55:51 +0300

    Tests added and updated

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import TickerDisplay from './components/TickerDisplay'
-import HomeScreen from './screens/HomeScreen'
+import TickerDisplay from './components/TickerDisplay';
+import TickerLineChart from './components/TickerLineChart';
+import HomeScreen from './screens/HomeScreen';
 import { fakeTicker } from './testUtils/testUtils';
 import {Provider} from 'react-redux';
-import store from './store'
+import store from './store';
 const tickers = fakeTicker();
 
 describe('<TickerDisplay/>', () => {
@@ -33,6 +34,23 @@ describe('<HomeScreen/>', () => {
 
   it('renders and matches a snapshot', () => {
     const { container , debug } = render(<Provider store={store}><HomeScreen/></Provider>);
+    debug();
+    expect(container).toMatchSnapshot();
+
+});
+   
+});
+
+describe('<TickerLineChart/>', () => {
+  it('renders out the button for changing refresh interval', () => {
+      const { container , debug } = render(<Provider store={store}><TickerLineChart/></Provider>);
+      debug();
+      expect(screen.getByText("Ticker price")).toBeInTheDocument();
+      
+  });
+
+  it('renders and matches a snapshot', () => {
+    const { container , debug } = render(<Provider store={store}><TickerLineChart/></Provider>);
     debug();
     expect(container).toMatchSnapshot();
 
